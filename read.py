305549f13e845eee96440a031de96cecd27c8758
vao1843 <vmanamino@gmail.com>
2017-07-27 16:36:24 +0200

    dataset added

--- read.py
+++ read.py
@@ -3,25 +3,32 @@ from openpyxl import Workbook
 from openpyxl.compat import range
 from openpyxl.utils import get_column_letter
 
-wb = load_workbook("dataset/xSL_test.xlsx")
+wb = load_workbook("dataset/uspDataset_current.xlsx", read_only=True)
 
 names = wb.get_sheet_names()
 
-wbR = Workbook()
+ws = wb[names[0]]
 
-s1 = wbR.create_sheet(title="results")
+print(ws['A1'].value)
 
-s1['A1'] = 'Header'
 
-row = 2
+# print(names)
 
-col = 1
+# wbR = Workbook()
 
-for name in names:
-	print(name)
-	s1.cell(column=col, row=row, value=name)
+# s1 = wbR.create_sheet(title="results")
 
-print(s1['A1'].value)
+# s1['A1'] = 'Header'
 
-wbR.save(filename="results/result.xlsx")
+# row = 2
+
+# col = 1
+
+# for name in names:
+# 	print(name)
+# 	s1.cell(column=col, row=row, value=name)
+
+# print(s1['A1'].value)
+
+# wbR.save(filename="results/result.xlsx")
 
