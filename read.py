cc2db93c824c832fd2e6f5bcb12b95dd152c0231
vao1843 <vmanamino@gmail.com>
2017-07-27 18:06:34 +0200

    openpyxl

--- read.py
+++ read.py
@@ -3,32 +3,19 @@ from openpyxl import Workbook
 from openpyxl.compat import range
 from openpyxl.utils import get_column_letter
 
-wb = load_workbook("dataset/uspDataset_current.xlsx", read_only=True)
+import string
+
+wb = load_workbook("dataset/uspDataset_current.xlsx")
 
 names = wb.get_sheet_names()
 
-ws = wb[names[0]]
+data = wb[names[0]]
 
-print(ws['A1'].value)
-
-
-# print(names)
-
-# wbR = Workbook()
-
-# s1 = wbR.create_sheet(title="results")
-
-# s1['A1'] = 'Header'
-
-# row = 2
-
-# col = 1
-
-# for name in names:
-# 	print(name)
-# 	s1.cell(column=col, row=row, value=name)
-
-# print(s1['A1'].value)
-
-# wbR.save(filename="results/result.xlsx")
+counter = 0
+for cell in next(data.rows):
+	counter += 1
+	if cell.value is not None:	
+		print(get_column_letter(counter))		
+		print(cell.value)
+		print("\n")
 
