122f3cf2139a7308acc263fb0999dff63770f027
vao1843 <vmanamino@gmail.com>
2017-08-04 12:39:14 +0200

    'isolation'

---
+++ tests/regexs.py
@@ -0,0 +1,17 @@
+import unittest
+import re
+
+# Here's our "unit tests".
+# class IsOddTests(unittest.TestCase):
+
+#     def testOne(self):
+#         self.failUnless(IsOdd(1))
+
+#     def testTwo(self):
+#         self.failIf(IsOdd(2))
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
